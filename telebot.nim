33cdfa7ee3c24307806220063f03d823c7bb3e53
Huy Doan <me@huy.im>
2019-05-10 11:46:23 +0700

    tweak: another attempt to speed up

--- telebot.nim
+++ telebot.nim
@@ -7,10 +7,7 @@ proc newTeleBot*(token: string): TeleBot =
   ## Init new Telegram Bot instance
   new(result)
   result.token = token
-  result.lastUpdateId = 0
-  result.updateCallbacks = @[]
   result.commandCallbacks = newTable[string, seq[CommandCallback]]()
-  result.inlineQueryCallbacks = @[]
 
 include telebot/api
 include telebot/events
