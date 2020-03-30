3e88f1fce526f43b825678681fe8e07fa9403ccb
Huy Doan <me@huy.im>
2020-03-31 03:47:29 +0700

    update echo bot example

--- examples/echo_bot.nim
+++ examples/echo_bot.nim
@@ -16,11 +16,7 @@ proc updateHandler(b: Telebot, u: Update) {.async.} =
 
 
 proc greatingHandler(b: Telebot, c: Command) {.async.} =
-  var message = newMessage(c.message.chat.id, "hello " & c.message.fromUser.get().firstName)
-  message.disableNotification = true
-  message.replyToMessageId = c.message.messageId
-  message.parseMode = "markdown"
-  discard b.send(message)
+  discard b.sendMessage(c.message.chat.id, "hello " & c.message.fromUser.get().firstname, disableNotification = true, replyToMessageId = c.message.messageId)
 
 when isMainModule:
   let bot = newTeleBot(API_KEY)
