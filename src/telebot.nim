afe4ad877ec015091f5b6fde32ecb2502fe59a3f
Huy Doan <me@huy.im>
2024-05-05 18:45:58 +0700

    Update telebot.nim

--- src/telebot.nim
+++ src/telebot.nim
@@ -6,13 +6,12 @@ export types, webhook, keyboard, inputmedia, helpers, api, events
 proc setProxy*(b: Telebot, url: string, auth = "") {.inline.} =
   b.proxy = newProxy(url, auth)
 
-proc newTeleBot*(token: string, serverUrl="https://api.telegram.org", errorHandler = defaultErrorHandler): TeleBot =
+proc newTeleBot*(token: string, serverUrl="https://api.telegram.org"): TeleBot =
   ## Init new Telegram Bot instance
   new(result)
   result.token = token
   result.serverUrl = serverUrl
   result.commandCallbacks = newTable[string, seq[CommandCallback]]()
-  result.errorHandler = errorHandler
 
 #import telebot/private/[]
 #export api, events
